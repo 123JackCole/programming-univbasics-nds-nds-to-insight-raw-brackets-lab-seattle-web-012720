2666b943b91da29b968348440d31c74c0cea8939
Jack Cole <123jack.cole@gmail.com>
2020-01-14 01:47:06 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,20 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  output_hash = {}
+  outer_index = 0
+  while outer_index < nds.count do
+    name = nds[outer_index][:name]
+    inner_index = 0
+    gross_total = 0
+    while inner_index < nds[outer_index][:movies].count do
+      gross_total += nds[outer_index][:movies][inner_index][:worldwide_gross]
+      inner_index += 1
+    end
+    output_hash[name] = gross_total
+    outer_index += 1
+  end
+  output_hash
+  
 end
