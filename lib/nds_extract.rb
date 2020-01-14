0e22bd16ac5346138aca210bd494942c4b0da910
Jack Cole <123jack.cole@gmail.com>
2020-01-14 01:41:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,10 +27,10 @@ def directors_totals(nds)
     gross_total = 0
     while inner_index < directors_database[outer_index][:movies].count do
       gross_total += directors_database[outer_index][:movies][inner_index][:worldwide_gross]
-      inner_index++ # += 1
+      inner_index += 1
     end
     output_hash[:#{name}] = gross_total
-    outer_index++ #+= 1
+    outer_index += 1
   end
   output_hash
   
